7ad669c1c3c4c35dcf5bfe9d0d0d2bdb530f270e
shashwat0756 <shashwatsandilya514@gmail.com>
2023-07-19 21:46:18 +0530

    Generating PDF

--- main1.py
+++ main1.py
@@ -1,14 +1,14 @@
 from fpdf import FPDF
+import pandas
 
 pdf = FPDF(orientation="P",unit="mm",format="A4")
 print(type(pdf))
-pdf.add_page()
-pdf.set_font(family="times",style="B",size = 12)
-pdf.cell(w=0,h=12,txt="Hello There!",align="L",ln=1)
-pdf.cell(w=0,h=12,txt="Hi There!",align="L",ln=1)
-pdf.add_page()
-pdf.set_font(family="times",style="B",size = 12)
-pdf.cell(w=0,h=12,txt="Hello There!",align="L",ln=1)
-pdf.cell(w=0,h=12,txt="Hi There!",align="L",ln=1)
+df = pandas.read_csv("topics.csv",sep=",")
+for index,row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="times",style="B",size = 24)
+    pdf.set_text_color(0,0,254)
+    pdf.cell(w=0,h=12,txt=row["Topic"],align="L",ln=1)
+    pdf.line(10,21,200,21)
 
 pdf.output("output.pdf")
